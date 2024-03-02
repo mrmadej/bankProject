08d1cd9e99f74fbb0d0e1b4164ef36408b753f8b
Marcin Madej <marcin.r.madej@gmail.com>
2024-03-02 15:18:22 +0100

    login/register panel

--- src/main/java/bank/bankProject/controller/BankClientController.java
+++ src/main/java/bank/bankProject/controller/BankClientController.java
@@ -1,7 +1,27 @@
 package bank.bankProject.controller;
 
+import bank.bankProject.service.BankClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("Client")
 public class BankClientController {
+    private BankClientService bankClientService;
+
+    @Autowired
+    public BankClientController(BankClientService bankClientService) {
+        this.bankClientService = bankClientService;
+    }
+
+    @GetMapping("loginPanel")
+    public String loginPanel() {
+        return "login-panel";
+    }
+    @GetMapping("registerPanel")
+    public String registerPanel() {
+        return "register-panel";
+    }
 }
